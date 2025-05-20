4873cc7c6353d0bdf5eccd062430a8833f3c2a9c
Rishabhpandey32 <143044371+Rishabhpandey32@users.noreply.github.com>
2025-05-20 22:32:34 +0530

    UI alignment done

--- NewMAUiApp/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/NewMAUiApp.AssemblyInfo.cs
+++ NewMAUiApp/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/NewMAUiApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewMAUiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+aa68abb2bca3a7bd5e3b56f1bc410b41c4a9c583")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewMAUiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewMAUiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
